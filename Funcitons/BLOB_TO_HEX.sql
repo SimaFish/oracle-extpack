85b0e7f05d9dea3454db8afd57fcde7da0d5759c
Andriy Oseledko <oseledko.a@gmail.com>
2019-10-02 13:44:05 +0300

    Minor fixes

--- Funcitons/BLOB_TO_HEX.sql
+++ Funcitons/BLOB_TO_HEX.sql
@@ -1,20 +1,35 @@
+-- =============================================================================
+--  Function      : BLOB_TO_HEX
+-- =============================================================================
+--  Author        : Andriy (Andrii) Oseledko
+--  Version       : 1.1
+--  Creation date : 19.08.2019
+--  Modifications : 02.10.2019
+-- =============================================================================
+--  Description   : Converts BLOB binary data into hexadecimal representation.
+--                : Each byte is represented by two hexadecimal (HEX) digits.
+--                : Resulting HEX returned as CLOB string.
+--                : 
+--                : This function is a bypass for Oracle RAWTOHEX, which doesn't
+--                : support conversion of BLOB value which resulting HEX exceeds
+--                : 32767 bytes limit of VARCHAR2 data type.
+-- =============================================================================
+--  Parameters    : > p_blob_val:
+--                : Any non-null BLOB hexadecimal value to convert into HEX.
+-- =============================================================================
+
 CREATE OR REPLACE FUNCTION BLOB_TO_HEX (p_blob_val IN BLOB)
 RETURN CLOB
 IS
     v_hex_code   CLOB;
+    v_blob_len   INTEGER;
+    v_currpos    INTEGER := 1;
     v_hex_buffer VARCHAR2(32000);
-    v_blob_len   PLS_INTEGER;
-    v_chunk_size INTEGER := 16000;
-    v_currpos    SIMPLE_INTEGER := 1;
+    v_chunk_size SIMPLE_INTEGER := 16000;
 BEGIN
-    /*
-    *   Title: Converts BLOB binary data into hexadecimal (HEX) representation
-    *   Author: Andriy Oseledko
-    *   Date: 19.08.2019
-    *   Code version: 1.0
-    */
     v_blob_len := DBMS_LOB.GETLENGTH(p_blob_val);
 
+    WHILE v_currpos <= v_blob_len
     LOOP
         v_hex_buffer := RAWTOHEX(DBMS_LOB.SUBSTR(lob_loc => p_blob_val,
                                                  amount  => v_chunk_size,
@@ -22,7 +37,6 @@ BEGIN
         v_hex_code := v_hex_code || v_hex_buffer;
 
         v_currpos := v_currpos + v_chunk_size;
-        EXIT WHEN v_currpos > v_blob_len;
     END LOOP;
 
     RETURN v_hex_code;
--- Funcitons/HEX_TO_BLOB.sql
+++ Funcitons/HEX_TO_BLOB.sql
@@ -1,20 +1,33 @@
+-- =============================================================================
+--  Function      : HEX_TO_BLOB
+-- =============================================================================
+--  Author        : Andriy (Andrii) Oseledko
+--  Version       : 1.1
+--  Creation date : 18.08.2019
+--  Modifications : 02.10.2019
+-- =============================================================================
+--  Description   : Converts CLOB containing hexadecimal representation (HEX)
+--                : of binary data into BLOB value.
+--                : 
+--                : This function is a bypass for Oracle HEXTORAW, which doesn't
+--                : support CLOB data directly, and conversion of HEX values
+--                : longer than 32767 bytes.
+-- =============================================================================
+--  Parameters    : > p_hex_code:
+--                : Any valid hexadecimal value to convert into binary data.
+-- =============================================================================
+
 CREATE OR REPLACE FUNCTION HEX_TO_BLOB (p_hex_code IN CLOB)
 RETURN BLOB
 IS
     v_blob_val    BLOB;
-    v_hexcode_len PLS_INTEGER;
     v_raw_buffer  RAW(32000);
-    v_chunk_size  INTEGER := 32000;
-    v_hex_code    CLOB := p_hex_code;
     v_currpos     SIMPLE_INTEGER := 1;
+    v_hexcode_len SIMPLE_INTEGER := 0;
+    v_chunk_size  SIMPLE_INTEGER := 32000;
+    v_hex_code    CLOB := p_hex_code;
 BEGIN
-    /*
-    *   Title: Converts CLOB containing hexadecimal representation (HEX) of binary data into BLOB value
-    *   Author: Andriy Oseledko
-    *   Date: 18.08.2019
-    *   Code version: 1.0
-    */
-    --Left padding (with '0') the HEX code if it has unpaired length
+    -- Left padding with '0' the HEX code if it has unpaired length
     v_hex_code := CASE MOD(DBMS_LOB.GETLENGTH(p_hex_code), 2)
                      WHEN 0 THEN p_hex_code
                      ELSE '0' || p_hex_code
@@ -24,23 +37,26 @@ BEGIN
                              cache   => FALSE,
                              dur     => DBMS_LOB.CALL);
 
-    DBMS_LOB.OPEN(lob_loc   => v_blob_val,
-                  open_mode => DBMS_LOB.LOB_READWRITE);
-
     v_hexcode_len := DBMS_LOB.GETLENGTH(v_hex_code);
 
+    WHILE v_currpos <= v_hexcode_len
     LOOP
-        v_raw_buffer := HEXTORAW(TO_CHAR(DBMS_LOB.SUBSTR(lob_loc => v_hex_code,
-                                                         amount  => v_chunk_size,
-                                                         offset  => v_currpos)));
+        v_raw_buffer := HEXTORAW(DBMS_LOB.SUBSTR(lob_loc => v_hex_code,
+                                                 amount  => v_chunk_size,
+                                                 offset  => v_currpos));
         DBMS_LOB.WRITEAPPEND(v_blob_val,
                              UTL_RAW.LENGTH(v_raw_buffer),
                              v_raw_buffer);
 
         v_currpos := v_currpos + v_chunk_size;
-        EXIT WHEN v_currpos > v_hexcode_len;
     END LOOP;
 
     RETURN v_blob_val;
 END HEX_TO_BLOB;
+/
+
+ALTER FUNCTION HEX_TO_BLOB
+    COMPILE PLSQL_CODE_TYPE = NATIVE
+    PLSQL_OPTIMIZE_LEVEL = 3
+    REUSE SETTINGS;
 /
